66528b6ead40b9495728d0037a4522cb5e9d5579
m.helfenstein@gmail.com <m.helfenstein@gmail.com>
2016-04-13 20:33:47 +0200

    Inital Test Projekt

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sharpberry
-Projekt with .Net C-Sharp on a Raspberry Pi
+> Projekt with .Net C-Sharp on a Raspberry Pi
+
+# Vorgehen Installation
+1. Download IoT Dashboard von https://developer.microsoft.com/en-us/windows/iot/GetStarted.htm
+2. IoT Dashboard Tool Ausführen und Windows IOT auf Flash Karte installieren
+3. Flash Karte in Raspberry stecken und booten (Dauert das 1. mal ziemlich lange)
+
+
+# .Net Projekt (13.04.2016)
+**Informationen**
+- Windows IoT Core Project Templates für VS2015 herunterladen https://visualstudiogallery.msdn.microsoft.com/55b357e1-a533-43ad-82a5-a88ac4b01dec
+- Samples usw zu finden unter: https://github.com/ms-iot
+
+**Projekt erstellen**
+- neues c# Proekt "Windows iot Core"
+- Universal Windows Target Version 10.0; Build 10586
+- Referenz hinzufügen "Windows IoT Extensions for the UWP" in Version 10586 https://blogs.msdn.microsoft.com/cdndevs/2015/05/08/windows-10-how-to-use-iot-extension-for-raspberry-pi-2-part-1/
+- 
---
+++ Source/IOBackgroundController/StartupTask.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.System.Threading;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace IOBackgroundController
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private const int PIN_NR_RED_LED = 35; //Red status light
+        private GpioPin m_Pin35RedLed;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to perform background work
+            //
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+            //Create the deferral by requesting it from the task instance.
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            //IO Zugriff initialisieren
+            InitGPIO();
+
+            await ExampleMethod();
+
+            //Once the asynchronous method(s) are done, close the deferral.
+            deferral.Complete();
+        }
+
+
+        #region " Sample Initial Test "
+
+        private void InitGPIO()
+        {
+            m_Pin35RedLed = GpioController.GetDefault().OpenPin(PIN_NR_RED_LED);
+            m_Pin35RedLed.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private async Task ExampleMethod()
+        {
+            int counter = 0;
+            GpioPinValue value = m_Pin35RedLed.Read();
+
+            do
+            {
+                //Invertieren des  on aktuellen Zustand
+                value = (value == GpioPinValue.High) ? GpioPinValue.Low : GpioPinValue.High;
+                m_Pin35RedLed.Write(value);
+
+                //Pause
+                await Task.Delay(500);
+            } while (counter < 10);
+        }
+
+        #endregion
+    }
+}
